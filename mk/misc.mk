1a125bc8233be670ee7ef59f29a87eabba6036c9
Alexander Indenbaum <aindenba@redhat.com>
2023-08-30 14:06:08 +0000

    NVMEOF_VERSION for cli
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

--- mk/misc.mk
+++ mk/misc.mk
@@ -1,7 +1,7 @@
 ## Miscellaneous:
 
 # nvmeof_cli
-NVMEOF_CLI = $(DOCKER_COMPOSE_ENV) $(DOCKER_COMPOSE) run --rm nvmeof-cli --server-address $(NVMEOF_IP_ADDRESS) --server-port $(NVMEOF_GW_PORT)
+NVMEOF_CLI = NVMEOF_VERSION=$(NVMEOF_VERSION) $(DOCKER_COMPOSE_ENV) $(DOCKER_COMPOSE) run --rm nvmeof-cli --server-address $(NVMEOF_IP_ADDRESS) --server-port $(NVMEOF_GW_PORT)
 
 alias: ## Print bash alias command for the nvmeof-cli. Usage: "eval $(make alias)"
 	@echo alias nvmeof-cli=\"$(NVMEOF_CLI)\"
